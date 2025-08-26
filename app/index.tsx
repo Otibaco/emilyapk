76c7767a0c8b826ea3bfc64905c0b35280cfe963
rdghostmann <randalchukzwilson@gmail.com>
2025-08-26 17:02:17 +0100

    Added Tabs II

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
+import { Redirect } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className="text-green-500 text-2xl font-bold">Welcome.</Text>
-    </View>
+    <Redirect href="/(tabs)/home" />
+
+    // <View className="flex-1 items-center justify-center ">
+    //   <Text className="text-green-500 text-2xl font-bold">Welcome.</Text>
+    // </View>
   );
 }
